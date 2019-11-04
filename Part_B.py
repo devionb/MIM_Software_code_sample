9c374bad3558c7d234b722840bbba416dd3d6d9a
devionb <35434561+devionb@users.noreply.github.com>
2019-11-04 14:48:43 -0500

    Truncate a string to 5 characters if length is % 5
    Example: 0123456789 will be 01234 since it was a multiple of 5.
    Example: 012345 will be 012345 since it is not a multiple of 5.

---
+++ Part_B.py
@@ -0,0 +1,14 @@
+# Devion Buchynsky 
+# Part B - Truncate teh string to 5 characters if its length is a multiple of 5. 
+# multiples of 5: 5,10,15,20...... 
+
+multiple_of_5_string = '0123456789' # 10 characters
+multiple_of_6_string = '012345' # 6 characters
+
+def truncate_string(string_1):
+    if len(string_1) % 5 == 0: # if the string is a % of 5 then it will truncate the string to 5 characters, if not it will not do anything.
+        return ''.join(string_1[0:5]) # [0:5] will only take 0-5 characters and not include the rest. 
+    return string_1
+
+print(truncate_string(multiple_of_5_string))
+print(truncate_string(multiple_of_6_string))
