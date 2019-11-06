7d43ce3471bb10916bc99209f27bec93a6360065
devionb <35434561+devionb@users.noreply.github.com>
2019-11-06 12:18:43 -0500

    Removes hyphen and appends new string
    Removes the hyphen at the end of the string and appends a new string to the first string.

--- Part_D.py
+++ Part_D.py
@@ -2,28 +2,16 @@
 # Part D - If the string ends with a hyphen, remove it, and append the next string in the list to the current one.
 
 hyphen_string = 'This sentence ends with a -'
-hyphen_string2 = ' hyphen.'
+hyphen_string2 = 'hyphen.'
 hyphen_character = ['-']
-newlist = ''
-
 
 
 #hyphen_string.append(hyphen_string2)
-#print(hyphen_string)
+print('Before removing the hyphen and appending the next string: ' + str(hyphen_string))
 
 for i in hyphen_character:
-    hyphen_string = hyphen_string.replace(i, '')
-print('Result is: ' +str(hyphen_string))
+    hyphen_string = hyphen_string.replace(i, '') #removing the hyphen
+    #hyphen_string.append(hyphen_string2)
+    hyphen_string3 = hyphen_string + hyphen_string2 # adding two strings together
+print('After removing a hyphen and appending the next string: ' +str(hyphen_string3))
 
-def remove_hyphen(hyphen_string):
-    for i in hyphen_string:
-        #if hyphen_character == hyphen_string.endswith('-'):
-            #hyphen_string = hyphen_string.replace('-', '')
-            #newlist.append(i.replace('-',''))
-            #hyphen_string.append(hyphen_string2)
-            print(hyphen_string)
-      
-        
-
-#print(remove_hyphen(hyphen_string))
-#print(remove_hyphen(hyphen_string2))
